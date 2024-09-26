604e31563caaef3ebe9fc0cab25af4e6117a7344
Shubham9607 <shubhamazm22@gmail.com>
2024-09-26 19:17:45 +0530

    .

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+
+function About() {
+  return (
+    <>
+    <div 
+    name="About"
+     className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-16'>
+      <div>
+      <h1 className='text-3xl font-bold mb-5 '>About</h1>
+      <p>
+          Hello, I'm Shubham, a dedicated Web Developer with a strong focus on the MERN Stack. 
+          With a solid IT background, I am committed to crafting impactful and visually striking 
+          software solutions that make a lasting impression.
+        </p>
+        <br/>
+        <h1 className='text-green-600 font-semibold text-xl'>
+            Education & Training
+            </h1>
+        <span>
+          B.Tech(C.S.E) , Dr. A.P.J. Abdul Kalam Technical University, Lucknow 2024
+        </span>
+        <br/>
+        <br/>
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in  C++ Experienced with react js, Express js, Node js, MongoDB, NPM , HTML5, CSS, Tailwind CSS, JavaScript,
+          Python, OPPS, Java.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+        Oasis Infobyte (Web Development) Intern 08/2023 - 09/2023 | India - Remote
+        Learned and developed new skills and gained hands-on knowledge in creating cutting-edge web solutions. 
+        Contributed my best, learned from industry experts and implemented practical knowledge through live 
+        projects and real-world examples.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my technical skills and creativity to deliver innovative 
+          software solutions that exceed client expectations and enhance the digital landscape. 
+          I am committed to continuous learning and growth, embracing challenges that push the 
+          boundaries of web development. My goal is to make a lasting impact through visually 
+          striking and functionally robust applications.
+        </p>
+      </div>
+
+    </div></>
+  )
+}
+
+export default About
---
+++ src/components/Experiance.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import html from "../../public/html.png";
+import css from "../../public/css.jpg";
+import java from "../../public/java.png";
+import javascript from "../../public/javascript.png";
+import oracle from "../../public/oracle.png";
+import spring from "../../public/spring.png";
+import springBoot from "../../public/springBoot.jpg";
+
+const Experience = () => {
+
+    const cardItem =[
+        {
+        id:1,
+        logo:html,
+        name:"MongoDB"
+        },
+        {
+            id:2,
+            logo:css,
+            name:"CSS"
+      },
+       {
+         id:3,
+         logo:java,
+         name:"Java"
+        }, 
+       {
+         id:4,
+         logo:javascript,
+         name:"JavaScript"
+         }, 
+         {
+             id:5,
+             logo:oracle,
+             name:"Oracle"
+          } ,
+           {
+            id:6,
+            logo:spring,
+            name:"Spring"
+            }     
+    ]
+  return (
+    <div name="Experience" 
+    className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-16'>
+     <div>
+        <h1 className='text-3xl font-bold mb-5 '>Experiance</h1>
+        <p >
+            I've more than 2 years of experiance in below technology.
+        </p>
+        <div className='grid grid-cols-2 md:grid-cols-5 gap-7 my-3 ' >
+            {
+                cardItem.map(({id,logo,name}) =>(
+                    <div className='flex flex-col items-center justify-center border-[2px] rounded-full md:w-[200px] md:h-[200px] p-1 cursor-pointer hover:scale-110' key={id}>
+                        <img src={logo} className='w-[150px] rounded-full' alt="" />
+                        <div>
+                            <div className='font-bold text-xl mb-2'>{name}</div>
+                        </div>  
+                      </div>  
+                ))
+            }
+        </div>
+     </div>
+    </div>
+  )
+}
+
+export default Experience
